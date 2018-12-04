ee2518cb7d51ef7691a42cbde1f0ab71fd694aa8
Neeraj Walia <31940257+NKWalia@users.noreply.github.com>
2018-12-03 23:19:02 -0500

    Add files via upload

--- Final_Project_Capstone.py
+++ Final_Project_Capstone.py
@@ -1,4 +1,8 @@
 
+# coding: utf-8
+
+# In[1]:
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -14,15 +18,31 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectPercentile
 
+
+# In[4]:
+
+
 df_train = pd.read_csv("train.csv")
 
+
+# In[5]:
+
 df_train.columns
+
+
+# In[6]:
 
 #descriptive statistics summary
 df_train['SalePrice'].describe()
 
+
+# In[7]:
+
 #Distribution of Sale Price of houses as histogram
 sns.distplot(df_train['SalePrice']);
+
+
+# In[8]:
 
 '''Skewness is usually described as a measure of a dataset’s symmetry – or lack of symmetry.   
 A perfectly symmetrical data set will have a skewness of 0.   The normal distribution has a skewness of 0.
@@ -37,11 +57,16 @@ Kurtosis is the degree of peakedness of a distribution” – Wolfram MathWorld
 print("Skewness: %f" % df_train['SalePrice'].skew())
 print("Kurtosis: %f" % df_train['SalePrice'].kurt())
 
+
+# In[9]:
+
 #scatter plot for Great living area vs saleprice.  It shows linear pattern.
 var = 'GrLivArea'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
 
+
+# In[10]:
 
 #Plot between Total Basement Square ft and Sale Price shows linear pattern.
 var = 'TotalBsmtSF'
@@ -49,11 +74,15 @@ data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
 
 
+# In[11]:
+
 #Year built of house has linear relation with Sale price of the house.
 var = 'YearBuilt'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
 
+
+# In[12]:
 
 #Remodeled yr of house has linear relation with Sale price of the house.
 var = 'YearRemodAdd'
@@ -61,16 +90,23 @@ data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
 
 
+# In[13]:
+
 #Linear relationship between 1st floor sq ft and sale price.
 var = '1stFlrSF'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000)); 
+
+
+# In[14]:
 
 #Linear relationship between 2st floor sq ft and sale price.
 var = '2ndFlrSF'
 data = pd.concat([df_train['SalePrice'], df_train[var]], axis=1)
 data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000)); 
 
+
+# In[15]:
 
 #Range of house price as per their year of built
 var = 'YearBuilt'
@@ -80,6 +116,9 @@ fig = sns.boxplot(x=var, y="SalePrice", data=data)
 fig.axis(ymin=0, ymax=800000);
 plt.xticks(rotation=90);
 
+
+# In[16]:
+
 #Compute pairwise correlation of columns, excluding NA/null values
 #correlation matrix
 corrmat = df_train.corr()
@@ -87,12 +126,19 @@ f, ax = plt.subplots(figsize=(12, 9))
 sns.heatmap(corrmat, vmax=.8, square=True);
 
 
+# In[17]:
+
 #Correlation of numerical variables with Sales Price
 corrmat.sort_values(["SalePrice"], ascending = False, inplace = True)
+print(corrmat.SalePrice)
 
+
+# In[18]:
 
 df_test = pd.read_csv("test.csv")
 
+
+# In[19]:
 
 #Missing Values
 
@@ -103,6 +149,8 @@ missing.sort_values(inplace=True)
 missing.plot.bar()
 
 
+# In[20]:
+
 #Number of missing values and its percentage
 total = df_train.isnull().sum().sort_values(ascending=False)
 percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascending=False)
@@ -110,6 +158,8 @@ percent = (df_train.isnull().sum()/df_train.isnull().count()).sort_values(ascend
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
 missing_data.head(20)
 
+
+# In[21]:
 
 #Missing Values for test data
 
@@ -119,6 +169,8 @@ missing.sort_values(inplace=True)
 missing.plot.bar()
 
 
+# In[22]:
+
 #Number of missing values and its percentage
 total = df_test.isnull().sum().sort_values(ascending=False)
 percent = (df_test.isnull().sum()/df_test.isnull().count()).sort_values(ascending=False)
@@ -126,6 +178,8 @@ percent = (df_test.isnull().sum()/df_test.isnull().count()).sort_values(ascendin
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
 missing_data.head(20)
 
+
+# In[23]:
 
 #Following columns are dropped and rest are kept as they are not acctually missing.
 #dealing with missing data
@@ -135,15 +189,24 @@ df_train = df_train.drop(['Id','LotFrontage','MasVnrType','MasVnrArea','GarageYr
 df_test = df_test.drop(['Id','LotFrontage','MasVnrType','MasVnrArea','GarageYrBlt'],1)
 
 
+# In[24]:
+
 df_test.dropna(subset=['MSZoning','BsmtHalfBath'])
 df_test=df_test.dropna(subset=['BsmtFullBath','GarageCars','GarageArea','BsmtFinSF1','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF'])
 
+
+# In[25]:
 
 #df_train.head()
 df_train.dropna(subset=['Electrical'])
 
 
+# In[26]:
+
 df_train.head()
+
+
+# In[27]:
 
 #Converting Categorical variables into binary
 #one_hot_encoded_training_predictors2 = pd.get_dummies(df_train)
@@ -158,14 +221,23 @@ final_train, final_test = one_hot_encoded_training_predictors.align(one_hot_enco
                                                                     join='left', 
                                                                     axis=1)
 
+
+# In[28]:
+
 final_test.head()
 
+
+# In[29]:
 
 final_train.head()
 
 
+# In[30]:
+
 final_test=final_test.drop(['SalePrice'],1)
 
+
+# In[31]:
 
 final_test.head()
 
@@ -173,6 +245,7 @@ final_test.head()
 final_test.fillna(0, inplace = True)
 
 
+# In[32]:
 
 #saleprice correlation matrix
 corrmat = final_train.corr()
@@ -185,8 +258,12 @@ hm = sns.heatmap(cm, cbar=True, annot=True, square=True, fmt='.2f', annot_kws={'
 plt.show()
 
 
+# In[33]:
 
 final_train.corr()
+
+
+# In[34]:
 
 #Split Data into Train and test.
 
@@ -195,6 +272,7 @@ y = final_train['SalePrice']
 #X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
 
+# In[35]:
 
 #Linear Regression Model
 lm = LinearRegression()
@@ -206,7 +284,18 @@ lm.fit(X, y)
 #pred=lm.predict(X_test)
 #lm.score(X_test, y_test)
 
-#np.mean((pred-y_test)**2)
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[40]:
 
 #Linear regression using cross validation
 score =cross_validation.cross_val_score(lm,X,y,cv=5, scoring='mean_squared_error')
@@ -214,8 +303,13 @@ mse_score = -score
 rmse_score = np.sqrt(mse_score)
 rmse_score.mean()
 
-lm.predict(final_test)
 
+# In[ ]:
+
+
+
+
+# In[42]:
 
 #Random Forest regression
 from sklearn.ensemble import RandomForestRegressor
@@ -228,14 +322,28 @@ mse_score1 = -score1
 rmse_score1 = np.sqrt(mse_score1)
 rmse_score1.mean()
 
+
+# In[43]:
+
 prediction=rf.predict(final_test)
+
+
+# In[44]:
 
 df_test['Predicted_SalePrice'] = prediction
 
 
+# In[45]:
+
 df_test.head()
 
 
+# In[265]:
+
+df_test.to_csv("test_prediction.csv", index=True, header=True)
+
+
+# In[ ]:
 
 
 
